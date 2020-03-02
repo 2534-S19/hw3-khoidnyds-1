e314902c06fcd518eaac3d08ad41a225dbb17a2c
khoidnyds <khoidnyds@vt.edu>
2020-03-02 17:30:17 -0500

    Final

--- homework3.c
+++ homework3.c
@@ -10,7 +10,7 @@ int main(void)
     unsigned int count1 = 0;
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    unsigned char history = UNPRESSED;
 
     // Stops the Watchdog timer.
     initBoard();
@@ -19,10 +19,10 @@ int main(void)
     initGPIO();
     // Initialize Timer0 to provide a one second count interval for updating LED2.
     // YOU MUST WRITE THIS FUNCTION IN myTimer.c
-    initTimer(TIMER32_0_BASE, TIMER0_PRESCALER, TIMER0_COUNT);
+    initTimer(TIMER0, TIMER0_PRESCALER, TIMER0_COUNT);
     // Initialize Timer1 to provide a one millisecond count interval for updating the button history.
     // YOU MUST WRITE THIS FUNCTION IN myTimer.c
-    initTimer(TIMER32_1_BASE, TIMER1_PRESCALER, TIMER1_COUNT);
+    initTimer(TIMER1, TIMER1_PRESCALER, TIMER1_COUNT);
 
     while(1)
     {
@@ -35,23 +35,21 @@ int main(void)
 
         // TODO: If Timer0 has expired, increment count0.
         // YOU MUST WRITE timer0expired IN myTimer.c
-
-
+        if(timer0Expired())
+            count0+=1;
 
         // TODO: If Timer1 has expired, update the button history from the pushbutton value.
         // YOU MUST WRITE timer1expired IN myTimer.c
-
-
+        if(timer1Expired()){ //XXXXXXX0 OR 0000000Y
+            history = (history << 1) | checkStatus_BoosterpackS1() ;
+        }
 
         // TODO: Call the button state machine function to check for a completed, debounced button press.
         // YOU MUST WRITE THIS FUNCTION BELOW.
-
-
-
+        bool pressed = fsmBoosterpackButtonS1(history);
         // TODO: If a completed, debounced button press has occurred, increment count1.
-
-
-
+        if(pressed)
+            count1+=1;
     }
 }
 
@@ -64,22 +62,127 @@ void initBoard()
 // Since count is an unsigned integer, you can mask the value in some way.
 void changeLaunchpadLED2(unsigned int count)
 {
-
+    int mode = count%8;
+    switch(mode){
+    case 1://red
+        turnOn_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 2://green
+        turnOff_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 3://yellow = red + green
+        turnOn_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 4://blue
+        turnOff_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 5://pink =  red + blue
+        turnOn_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 6://cyan = blue + green
+        turnOff_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 7://white
+        turnOn_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    default://off
+        turnOff_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    }
 }
 
 // TODO: Maybe the value of a count variable to a color for the Boosterpack LED
 // This is essentially a copy of the previous function, using a different LED
 void changeBoosterpackLED(unsigned int count)
 {
-
+    unsigned int mode = count%8;
+    switch(mode){
+    case 0: // off
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+    case 1://red
+        turnOn_BoosterpackLEDRed();
+        turnOff_BoosterpackLEDGreen();
+        turnOff_BoosterpackLEDBlue();
+        break;
+    case 2://green
+        turnOff_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOff_BoosterpackLEDBlue();
+        break;
+    case 3://yellow = red + green
+        turnOn_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOff_BoosterpackLEDBlue();
+        break;
+    case 4://blue
+        turnOff_BoosterpackLEDRed();
+        turnOff_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
+        break;
+    case 5://pink =  red + blue
+        turnOn_BoosterpackLEDRed();
+        turnOff_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
+        break;
+    case 6://cyan = blue + green
+        turnOff_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
+        break;
+    case 7://white
+        turnOn_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
+        break;
+    default://off
+        turnOff_BoosterpackLEDRed();
+        turnOff_BoosterpackLEDGreen();
+        turnOff_BoosterpackLEDBlue();
+        break;
+    }
 }
 
 // TODO: Create a button state machine.
 // The button state machine should return true or false to indicate a completed, debounced button press.
-bool fsmBoosterpackButtonS1(unsigned int buttonhistory)
+bool fsmBoosterpackButtonS1(unsigned char buttonhistory)
 {
     bool pressed = false;
+    typedef enum {up, down} button_state;
+    static button_state currentState = up;
 
-
+    switch(currentState){
+    case up:
+        if(buttonhistory == PRESSED){
+            pressed=true;
+            currentState = down;
+        }
+        break;
+    case down:
+        if(buttonhistory == UNPRESSED){
+            pressed=false;
+            currentState = up;
+        }
+        break;
+    }
     return pressed;
 }
+
--- myTimer.c
+++ myTimer.c
@@ -14,17 +14,13 @@
 void initTimer(unsigned int timer, unsigned int preScaler, unsigned int count)
 {
     // For the specified timer and pre-scale value, put the timer in 32-bit periodic mode.
-    Timer32_initModule(TIMER0,TIMER0_PRESCALER,TIMER32_32BIT,TIMER32_PERIODIC_MODE);
-    Timer32_initModule(TIMER1,TIMER1_PRESCALER,TIMER32_32BIT,TIMER32_FREE_RUN_MODE);
+    Timer32_initModule(timer,preScaler,TIMER32_32BIT,TIMER32_PERIODIC_MODE);
 
     // For the specified timer, pass the count value.
-    Timer32_setCount(TIMER0,TIMER0_COUNT);
-    Timer32_setCount(TIMER1,TIMER1_COUNT);
+    Timer32_setCount(timer,count);
 
     // For the specified timer, configure the timer to repeat once it elapses.
-    Timer32_startTimer(TIMER0, REPEAT);
-    Timer32_startTimer(TIMER1, ONESHOT);
-
+    Timer32_startTimer(timer, REPEAT);
 }
 
 // TODO: Write a function that indicates whether Timer0 has expired.
@@ -39,7 +35,6 @@ bool timer0Expired(void)
     expired = (current > previous);
     previous = current;
     return expired;
-
 }
 
 // TODO: Write a function that indicates whether Timer1 has expired.
